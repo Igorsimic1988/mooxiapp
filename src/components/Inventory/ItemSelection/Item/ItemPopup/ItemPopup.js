d7522eef0dc2cc4ce0de9893e91ce1f333aeafbf
Igor Simic <igorsimic29081988@gmail.com>
2024-11-08 05:42:34 -0500

    settign some metadata and favicons

--- src/components/Inventory/ItemSelection/Item/ItemPopup/ItemPopup.js
+++ src/components/Inventory/ItemSelection/Item/ItemPopup/ItemPopup.js
@@ -1,8 +1,8 @@
 // src/components/Inventory/ItemSelection/Item/ItemPopup.js
 
-import React from 'react';
-import styles from './ItemPopup.module.css';
-import { ReactComponent as CloseIcon } from '../../../../../assets/icons/Close.svg'; // Adjust the path as needed
+import React from "react";
+import styles from "./ItemPopup.module.css";
+import { ReactComponent as CloseIcon } from "../../../../../assets/icons/Close.svg"; // Adjust the path as needed
 
 function ItemPopup({ item, onClose }) {
   return (
@@ -23,8 +23,21 @@ function ItemPopup({ item, onClose }) {
         {/* Content */}
         <div className={styles.content}>
           {/* Add your popup content here */}
-
-          
+          <div className={styles.itemgroup}>
+            <div className={styles.furnitureoutline}>
+              <div className={styles.furniturewrapper}>
+                <img
+                  src={item.src}
+                  alt={item.name}
+                  className={styles.itemImage}
+                />
+              </div>
+            </div>
+            <div className="furnituretextgroup">
+            <p className={styles.itemName}>{item.name}</p>
+             
+          </div>
+          </div>
         </div>
       </div>
     </div>

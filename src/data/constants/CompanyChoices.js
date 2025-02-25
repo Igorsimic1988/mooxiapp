18f19a3cc767e12a5819054ec97f39feb6267f33
stefan_co <cojics@yahoo.com>
2025-02-25 14:46:30 +0100

    rm cra app

--- src/data/constants/CompanyChoices.js
+++
@@ -1,8 +0,0 @@
-const CompanyChoices = [
-    { id: 1, name: 'Citywide Relocation' },
-    { id: 2, name: 'Ace Moving & Storage' },
-    { id: 3, name: 'Best Rate Movers' },
-  ];
-  
-  export default CompanyChoices;
-  

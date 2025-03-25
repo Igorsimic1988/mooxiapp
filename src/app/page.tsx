b9be1602c3407582038f4bbd8f53c52cd6c6d735
Igor Simic <igorsimic29081988@gmail.com>
2025-03-25 16:09:44 +0100

    Update frontend before merging to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,14 @@
-'use client';
-import './globals.css';
-import React, { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/navigation';
-
-
-
-const DynamicLeads = dynamic(
-  () => import('./components/Leads/Leads'),
-  { ssr: false }
-)
+"use client";
+import "./globals.css";
+import React from "react";
+import Leads from "./components/Leads/Leads";
 
 function App() {
-  const router = useRouter();
-
-
-  const [accessToken, setAccessToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const token = localStorage.getItem("access-token");
-    setAccessToken(token);
-    if (token) {
-      router.push('/leads');
-    }
-  }, []);
-
-  const handleSignOut = () => {
-    localStorage.removeItem("access-token"); 
-    window.location.reload(); 
-  };
-
-
   return (
     <div className="App">
-      {accessToken ? (
-        <>
-        <button onClick={handleSignOut}>Sign Out</button>
-        <DynamicLeads /> 
-        </>
-      ) : (
-        <>
-          <a href="/register">Register</a> | <a href="/login">Login</a>
-        </>
-      )}
+      <Leads />
     </div>
   );
 }
 
 export default App;
-
